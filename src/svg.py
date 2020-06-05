55491a5561e7212a649e35748509d195b57964af
rob <rob@codebox.org.uk>
2020-06-05 18:28:13 +0100

    drawing in svg

--- src/svg.py
+++ src/svg.py
@@ -15,9 +15,17 @@ class Svg:
 
         self.styles.append(u'{0}{{{1}}}'.format(selector, ''.join(styles_txt)))
 
-    def add_text(self, text, x, y, style_class):
-        self.content.append(u'<text x="{}" y="{}" class="{}">{}</text>'.format(x, y, style_class, text))
+    def add_text(self, text, x, y, css_class):
+        self.content.append(u'<text x="{}" y="{}" class="{}">{}</text>'.format(x, y, css_class, text))
 
+    def add_centered_text(self, text, x, y, css_class):
+        self.content.append(u'<text x="{}" y="{}" text-anchor="middle" class="{}">{}</text>'.format(x, y, css_class, text))
+
+    def add_rect(self, x, y, h, w, css_class):
+        self.content.append(u'<rect x="{}" y="{}" width="{}" height="{}" class="{}"/>'.format(x, y, h, w, css_class))
+
+    def add_circle(self, x, y, r, css_class):
+        self.content.append(u'<circle cx="{}" cy="{}" r="{}" class="{}"/>'.format(x, y, r, css_class))
 
     def save(self, out_file):
         part1, tmp = self.template.split('%style%')
--- src/svg_wrapper.py
+++ src/svg_wrapper.py
@@ -1,10 +1,33 @@
 from field_names import *
-
+from svg_config import *
+import random
 class SvgWrapper:
     def __init__(self, svg):
         self.svg = svg
         self.star_count = 0
+        self.svg.add_styles('.starName', {'font-size': str(SVG_SMALL_TEXT_SIZE) + 'px'})
 
     def add_star(self, star):
-        self.svg.add_text(star[STAR_NAME], 50, self.star_count*20, '')
+        box_x, box_y, box_w, box_h = self._get_box_for_star(self.star_count)
+        self.svg.add_rect(box_x, box_y, box_w, box_h, 'starClass')
+
+        star_disc_width = 2
+        system_center_x = box_x + box_w/2
+        system_center_y = box_y + box_w/2
+        self.svg.add_circle(system_center_x, system_center_y, star_disc_width / 2, 'starDisc')
+
+        for planet in star[PLANETS]:
+            orbit_width = random.random() * 40
+            self.svg.add_circle(system_center_x, system_center_y, orbit_width/2, 'planetOrbit')
+
+        self.svg.add_centered_text(star[STAR_NAME], box_x + box_w/2, box_y + box_h - SVG_SMALL_TEXT_SIZE, 'starName')
         self.star_count += 1
+
+    def _get_box_for_star(self, star_index):
+        box_width = ((SVG_WIDTH - (2 * SVG_MARGIN_X)) / SVG_BOX_COUNT_X) - 2 * SVG_BOX_MARGIN_X
+        box_height = box_width * 1.5 #TODO
+
+        box_x = SVG_MARGIN_X + (star_index % SVG_BOX_COUNT_X) * (box_width + 2 * SVG_BOX_MARGIN_X) + SVG_BOX_MARGIN_X
+        box_y = SVG_MARGIN_Y + int(star_index / SVG_BOX_COUNT_X) * (box_height + 2 * SVG_BOX_MARGIN_Y) + SVG_BOX_MARGIN_Y
+
+        return box_x, box_y, box_width, box_height
